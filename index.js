95ba25c6050e498d6fa986db2808139662667125
Kelly online <kellyonline2016@gmail.com>
2020-05-12 11:53:51 +0100

    fix: comments

--- index.js
+++ index.js
@@ -5,12 +5,15 @@ var restify = require('restify');
 var server = restify.createServer();
 
 server.use(restify.plugins.acceptParser(server.acceptable));
-
+server.use(restify.plugins.queryParser());
+server.use(restify.plugins.jsonBodyParser());
 server.use(restify.plugins.bodyParser());
 
 
 var users = {};
 var max_user_id = 0;
+
+// retrieve default route path
 
 server.get('/', (req, res, next) =>{
     res.writeHead(200, {'Content-Type' : 'Application/json'});
@@ -19,17 +22,19 @@ server.get('/', (req, res, next) =>{
     return next()
 })
 
+
+//create new users under the user route with new id
+
 server.post('/user', (req, res, next) =>{
 
     var user = req.params;
-    max_user_id++;
-    user.id = max_user_id;
-    users[user.id] = user;
-    // console.log(typeof(user));
-    res.setHeader('content-type', 'application/json');
+	max_user_id++;
+	user.id = max_user_id;
+	users[user.id] = user;
+	res.setHeader('content-type', 'application/json');
 	res.writeHead(200);
-    res.end(JSON.stringify(user));
-    return next()
+	res.end(JSON.stringify(user));
+	return next();
 
 
     
