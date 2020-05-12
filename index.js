a5e46167923f641ee587f410b086a9f25f142bf7
Kelly online <kellyonline2016@gmail.com>
2020-05-12 01:10:34 +0100

    added endpoints

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 var restify = require('restify');
 
-function respond(req, res, next) {
-  res.send('hello ' + req.params.name);
-  next();
-}
 
 var server = restify.createServer();
-server.get('/hello/:name', respond);
-server.head('/hello/:name', respond);
+
+var users = {};
+var max_user_id = 0;
+
+server.get('/', (req, res, next) =>{
+    res.writeHead(200, {'Content-Type' : 'Application/json'});
+    res.end(JSON.stringify(users));
+
+    return next()
+})
+
+server.post('/user', (req, res, next) =>{
+
+    var user = req.params;
+    max_user_id++;
+
+    user.id = max_user_id;
+
+    users[user.id] = user;
+    // console.log(typeof(user));
+    
+
+    res.writeHead(200, {'Content-Type' : 'Application/json'});
+    res.end(JSON.stringify(user));
+
+    return next()
+})
+
+
 
 server.listen(8080, function() {
   console.log('%s listening at %s', server.name, server.url);
