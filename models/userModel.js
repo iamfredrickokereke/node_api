66329b331879e630f3895770c6cb311b26b72940
Kelly online <kellyonline2016@gmail.com>
2020-05-14 13:34:24 +0100

    feat: added models value schemas

--- models/userModel.js
+++ models/userModel.js
@@ -4,5 +4,9 @@ var Schema = mongoose.Schema,
     ObjectId = Schema.ObjectId;
 
 var UserShema = new Schema({
-
+    id : ObjectId,
+    firstname : String,
+    lastname : String,
+    email : String,
+    career : String
 })
