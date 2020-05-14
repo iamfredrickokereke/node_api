6b27fb0d9553d160055dc7d7bc8b7325045ad4d8
Kelly online <kellyonline2016@gmail.com>
2020-05-14 13:16:29 +0100

    added mongodb connection string

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,6 +1,6 @@
 module.exports{
     getMongoConnection: function () {
         //mongodb connection string
-        return ''
+        return 'mongodb+srv://eric:eric111@mycluster-dex7f.mongodb.net/test?retryWrites=true&w=majority';
     }
 }
